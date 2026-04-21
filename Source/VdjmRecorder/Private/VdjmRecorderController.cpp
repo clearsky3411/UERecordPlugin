10fdf98494cad5d082ca6c82799214236e37211d
clearsky3411 <clearsky3411@gmail.com>
2026-04-21 11:38:03 +0900

    Add UObject recorder option controller with bridge-request apply flow

---
+++ Source/VdjmRecorder/Private/VdjmRecorderController.cpp
@@ -0,0 +1,157 @@
+#include "VdjmRecorderController.h"
+
+#include "VdjmRecorderCore.h"
+
+UVdjmRecorderController* UVdjmRecorderController::CreateRecorderController(UObject* WorldContextObject)
+{
+	if (WorldContextObject == nullptr)
+	{
+		return nullptr;
+	}
+
+	UWorld* world = WorldContextObject->GetWorld();
+	if (world == nullptr)
+	{
+		return nullptr;
+	}
+
+	UVdjmRecorderController* controller = NewObject<UVdjmRecorderController>(WorldContextObject);
+	if (controller == nullptr)
+	{
+		return nullptr;
+	}
+
+	controller->CachedWorld = world;
+	controller->InitializeController();
+	return controller;
+}
+
+bool UVdjmRecorderController::InitializeController()
+{
+	if (!EnsureBridge())
+	{
+		return false;
+	}
+
+	WeakDataAsset = AVdjmRecordBridgeActor::TryGetRecordEnvConfigure();
+	return true;
+}
+
+bool UVdjmRecorderController::ApplyOptionRequest(const FVdjmRecorderOptionRequest& Request, FString& OutErrorReason)
+{
+	OutErrorReason.Reset();
+	if (!EnsureBridge())
+	{
+		OutErrorReason = TEXT("Recorder bridge actor is not available.");
+		return false;
+	}
+
+	if (!ValidateRequest(Request, OutErrorReason))
+	{
+		return false;
+	}
+
+	AVdjmRecordBridgeActor* bridge = WeakBridgeActor.Get();
+	if (bridge == nullptr)
+	{
+		OutErrorReason = TEXT("Recorder bridge actor is null.");
+		return false;
+	}
+
+	if (Request.bOverrideQualityTier)
+	{
+		bridge->SetRequestedQualityTier(Request.QualityTier);
+	}
+
+	if (Request.bOverrideFileName)
+	{
+		bridge->SetCurrentFileName(Request.FileName);
+	}
+
+	return true;
+}
+
+bool UVdjmRecorderController::StartRecording()
+{
+	if (!EnsureBridge())
+	{
+		return false;
+	}
+
+	if (AVdjmRecordBridgeActor* bridge = WeakBridgeActor.Get())
+	{
+		bridge->StartRecording();
+		return true;
+	}
+
+	return false;
+}
+
+void UVdjmRecorderController::StopRecording()
+{
+	if (!EnsureBridge())
+	{
+		return;
+	}
+
+	if (AVdjmRecordBridgeActor* bridge = WeakBridgeActor.Get())
+	{
+		bridge->StopRecording();
+	}
+}
+
+AVdjmRecordBridgeActor* UVdjmRecorderController::GetBridgeActor() const
+{
+	return WeakBridgeActor.Get();
+}
+
+UVdjmRecordEnvDataAsset* UVdjmRecorderController::GetResolvedDataAsset() const
+{
+	return WeakDataAsset.Get();
+}
+
+UWorld* UVdjmRecorderController::GetWorld() const
+{
+	return CachedWorld.Get();
+}
+
+bool UVdjmRecorderController::EnsureBridge()
+{
+	if (WeakBridgeActor.IsValid())
+	{
+		return true;
+	}
+
+	UWorld* world = CachedWorld.Get();
+	if (world == nullptr)
+	{
+		return false;
+	}
+
+	WeakBridgeActor = AVdjmRecordBridgeActor::TryGetRecordBridgeActor(world);
+	return WeakBridgeActor.IsValid();
+}
+
+bool UVdjmRecorderController::ValidateRequest(const FVdjmRecorderOptionRequest& Request, FString& OutErrorReason) const
+{
+	if (!Request.bOverrideQualityTier && !Request.bOverrideFileName)
+	{
+		OutErrorReason = TEXT("Option request is empty.");
+		return false;
+	}
+
+	if (Request.bOverrideQualityTier && Request.QualityTier == EVdjmRecordQualityTiers::EUndefined)
+	{
+		OutErrorReason = TEXT("Quality tier cannot be Undefined.");
+		return false;
+	}
+
+	if (Request.bOverrideFileName && Request.FileName.TrimStartAndEnd().IsEmpty())
+	{
+		OutErrorReason = TEXT("File name cannot be empty.");
+		return false;
+	}
+
+	return true;
+}
+
---
+++ Source/VdjmRecorder/Public/VdjmRecorderController.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "VdjmRecordTypes.h"
+#include "VdjmRecorderController.generated.h"
+
+class AVdjmRecordBridgeActor;
+class UVdjmRecordEnvDataAsset;
+
+USTRUCT(BlueprintType)
+struct VDJMRECORDER_API FVdjmRecorderOptionRequest
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Recorder|Option")
+	bool bOverrideQualityTier = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Recorder|Option")
+	EVdjmRecordQualityTiers QualityTier = EVdjmRecordQualityTiers::EDefault;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Recorder|Option")
+	bool bOverrideFileName = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Recorder|Option")
+	FString FileName;
+};
+
+UCLASS(BlueprintType)
+class VDJMRECORDER_API UVdjmRecorderController : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Recorder|Controller", meta = (WorldContext = "WorldContextObject"))
+	static UVdjmRecorderController* CreateRecorderController(UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category = "Recorder|Controller")
+	bool InitializeController();
+
+	UFUNCTION(BlueprintCallable, Category = "Recorder|Controller")
+	bool ApplyOptionRequest(const FVdjmRecorderOptionRequest& Request, FString& OutErrorReason);
+
+	UFUNCTION(BlueprintCallable, Category = "Recorder|Controller")
+	bool StartRecording();
+
+	UFUNCTION(BlueprintCallable, Category = "Recorder|Controller")
+	void StopRecording();
+
+	UFUNCTION(BlueprintPure, Category = "Recorder|Controller")
+	AVdjmRecordBridgeActor* GetBridgeActor() const;
+
+	UFUNCTION(BlueprintPure, Category = "Recorder|Controller")
+	UVdjmRecordEnvDataAsset* GetResolvedDataAsset() const;
+
+protected:
+	virtual UWorld* GetWorld() const override;
+
+private:
+	bool EnsureBridge();
+	bool ValidateRequest(const FVdjmRecorderOptionRequest& Request, FString& OutErrorReason) const;
+
+	TWeakObjectPtr<UWorld> CachedWorld;
+	TWeakObjectPtr<AVdjmRecordBridgeActor> WeakBridgeActor;
+	TWeakObjectPtr<UVdjmRecordEnvDataAsset> WeakDataAsset;
+};
+
